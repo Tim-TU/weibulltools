bc1e90d80645813ef54747cc860945b4495ba3f3
David Barkemeyer <david.barkemeyer@gmx.de>
2020-10-31 17:06:33 +0100

    Beispiel für reliability_data

--- R/interface.R
+++ R/interface.R
@@ -9,6 +9,22 @@
 #' @param event The name of a column holding binary data (0 or 1) indicating
 #' whether unit \emph{i} is a right censored observation (= 0) or a failure (= 1).
 #' @param id The name of a column holding the identification for every unit.
+#'
+#' @examples
+#' # Alloy T7987 dataset taken from Meeker and Escobar(1998, p. 131)
+#' cycles   <- c(300, 300, 300, 300, 300, 291, 274, 271, 269, 257, 256, 227, 226,
+#'               224, 213, 211, 205, 203, 197, 196, 190, 189, 188, 187, 184, 180,
+#'               180, 177, 176, 173, 172, 171, 170, 170, 169, 168, 168, 162, 159,
+#'               159, 159, 159, 152, 152, 149, 149, 144, 143, 141, 141, 140, 139,
+#'               139, 136, 135, 133, 131, 129, 123, 121, 121, 118, 117, 117, 114,
+#'               112, 108, 104, 99, 99, 96, 94)
+#' state <- c(rep(0, 5), rep(1, 67))
+#' id <- "XXXXXX"
+#'
+#' df <- data.frame(cycles = cycles, state = state, id = id)
+#'
+#' data <- reliability_data(df, x = cycles, event = state, id = id)
+#'
 #' @export
 reliability_data <- function(data, x, event, id) {
   data <- dplyr::select(data, x = {{x}}, event = {{event}}, id = {{id}})
