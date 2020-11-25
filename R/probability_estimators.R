1669da00da7a3dea0052e21625930fdaf3098823
David Barkemeyer <david.barkemeyer@gmx.de>
2020-11-25 14:40:37 +0100

    Closes #122

--- R/probability_estimators.R
+++ R/probability_estimators.R
@@ -53,12 +53,6 @@
 #'   \item \code{mr_ties.method}: \code{c("max", "min", "average")}, default is "max".
 #' }
 #'
-#' @param data A tibble returned by \link{reliability_data}.
-#' @param methods One or multiple methods of \code{"mr"}, \code{"johnson"},
-#'   \code{"kaplan"} or \code{"nelson"} used for estimating the failure
-#'   probabilities. See 'Details'.
-#' @param options A list of named options. See 'Options'.
-#'
 #' @return A tibble containing the following columns:
 #' \itemize{
 #'   \item \code{id}: Identification.
@@ -73,8 +67,62 @@
 #' }
 #'
 #' @export
-estimate_cdf <- function(
-  data, methods, options = list()
+estimate_cdf <- function(x, ...) {
+  UseMethod("estimate_cdf")
+}
+
+
+
+#' Estimation of Failure Probabilities
+#'
+#' @inherit estimate_cdf description details return
+#' @inheritSection estimate_cdf Options
+#'
+#' @param x A numeric vector which consists of lifetime data. Lifetime
+#'   data could be every characteristic influencing the reliability of a product,
+#'   e.g. operating time (days/months in service), mileage (km, miles), load
+#'   cycles.
+#' @param status A vector of binary data (0 or 1) indicating whether unit \emph{i}
+#'   is a right censored observation (= 0) or a failure (= 1).
+#' @param id A character vector for the identification of every unit.
+#' @param methods One or multiple methods of \code{"mr"}, \code{"johnson"},
+#'   \code{"kaplan"} or \code{"nelson"} used for estimating the failure
+#'   probabilities. See 'Details'.
+#' @param options A list of named options. See 'Options'.
+#'
+#' @export
+estimate_cdf.default <- function(x,
+                     status,
+                     id = rep("XXXXXX", length(x)),
+                     methods,
+                     options,
+                     ...
+) {
+  data <- reliability_data(x = x, status = status, id = id)
+
+  estimate_cdf.reliability_data(
+    data = data,
+    methods = methods,
+    options = options
+  )
+}
+
+
+
+#' Estimation of Failure Probabilities
+#'
+#' @inherit estimate_cdf description details return
+#' @inheritSection estimate_cdf Options
+#'
+#' @param data A tibble returned by \link{reliability_data}.
+#' @param methods One or multiple methods of \code{"mr"}, \code{"johnson"},
+#'   \code{"kaplan"} or \code{"nelson"} used for estimating the failure
+#'   probabilities. See 'Details'.
+#' @param options A list of named options. See 'Options'.
+#'
+#' @export
+estimate_cdf.reliability_data <- function(
+  data, methods, options = list(), ...
 ) {
 
   if (!inherits(data, "reliability_data")) {
@@ -123,12 +171,8 @@ estimate_cdf <- function(
 #'   \item "invbeta"; Exact Median Ranks using the inverse beta distribution
 #' }
 #'
-#' @param x A numeric vector which consists of lifetime data. Lifetime
-#'   data could be every characteristic influencing the reliability of a product,
-#'   e.g. operating time (days/months in service), mileage (km, miles), load
-#'   cycles.
+#' @inheritParams estimate_cdf.default
 #' @param status A vector of ones indicating that every unit \emph{i} has failed.
-#' @param id A character vector for the identification of every unit.
 #' @param method Method for the estimation of the cdf. Can be "benard" (default)
 #' or "invbeta".
 #' @param ties.method A character string specifying how ties are treated, default is "max".
